b4afb4aa77c398795e658782b46e561dc1482d62
karins28 <karins1010@gmail.com>
2021-03-31 19:09:52 +0300

    pass subscribe as parameter

--- mailChimpAutomation.py
+++ mailChimpAutomation.py
@@ -15,6 +15,9 @@ def getHashedEmail(email):
 
 # only tag is tel aviv
 # add recurring tag if you want 'nudnik', it'll send a mail three days after
-def createMember(email, firstName, lastName, tags=['tel aviv']):
-    api.lists.members.create_or_update(LIST_ID,  subscriber_hash=getHashedEmail(email), data={'email_address': email, 'status': 'subscribed', 'tags': tags,
-                                            'merge_fields': {'FNAME': firstName, 'LNAME': lastName, }});
+def createMember(email, firstName, lastName, status='subscribed', tags=['tel aviv']):
+    api.lists.members.create_or_update(LIST_ID,  subscriber_hash=getHashedEmail(email), data={'email_address': email, 'status': status, 'tags': tags, 'status_if_new': status,
+                                            'merge_fields': {'FNAME': firstName, 'LNAME': lastName,  }});
+
+
+
