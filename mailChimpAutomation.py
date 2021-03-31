39baa3580fed435c22a56eba01adc531a6055b91
karins28 <karins1010@gmail.com>
2021-03-31 14:31:37 +0300

    added tags

--- mailChimpAutomation.py
+++ mailChimpAutomation.py
@@ -10,4 +10,4 @@ LIST_ID = os.getenv("LIST_ID")
 api = mailchimp3.MailChimp(API_KEY)
 
 #can add email and names from the list
-api.lists.members.create(LIST_ID, data={'email_address': 'rebeccacaine42@gmail.com', 'status': 'subscribed'})
+api.lists.members.create(LIST_ID, data={'email_address': 'rebeccacaine42@gmail.com', 'status': 'subscribed', 'tags': ['tel aviv']})
